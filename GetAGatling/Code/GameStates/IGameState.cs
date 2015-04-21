e8352d56debd1c0ea2328c1296b2609e55800ede
Gnaarf <gnaarf@gmx.de>
2015-04-21 22:10:27 +0200

    Initial SetUp

---
+++ GetAGatling/Code/GameStates/IGameState.cs
@@ -0,0 +1,10 @@
+﻿using SFML.Graphics;
+
+namespace GameProject2D
+{
+    interface IGameState
+    {
+        GameState update();
+        void draw(RenderWindow win, View view);
+    }
+}
---
+++ GetAGatling/Code/GameStates/MainMenu/MainMenu.cs
@@ -0,0 +1,30 @@
+﻿using SFML.Graphics;
+using SFML.Window;
+
+namespace GameProject2D
+{
+    class MainMenu : IGameState
+    {
+        Sprite background;
+
+        public MainMenu()
+        {
+            background = new Sprite(new Texture("Textures/MainMenu_Background.jpg"));
+        }
+
+        public GameState update()
+        {
+            if (Keyboard.IsKeyPressed(Keyboard.Key.Return))
+            {
+                return GameState.InGame;
+            }
+
+            return GameState.MainMenu;
+        }
+
+        public void draw(RenderWindow win, View view)
+        {
+            win.Draw(background);
+        }
+    }
+}
