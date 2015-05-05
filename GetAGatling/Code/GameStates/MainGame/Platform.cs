44fc98baea89fc4572d1e60f41c44c0705725496
PStang <paul@stacom.de>
2015-05-05 17:53:20 +0200

    Start working on Platform

--- GetAGatling/Code/GameStates/MainGame/Platform.cs
+++ GetAGatling/Code/GameStates/MainGame/Platform.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Box2DX.Collision;
+using Box2DX.Dynamics;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,17 @@ namespace GetAGatling.Code.GameStates.MainGame
 {
     class Platform
     {
+        BodyDef bodydef;
+        Body body;
+        PolygonDef shapedef;
+
+        public Platform(World world, float positionX, float positionY, float heightX, float heightY) 
+        {
+            bodydef = new BodyDef();
+            bodydef.Position.Set(positionX / 30.0F, positionY / 30.0F);
+
+            body = world.CreateBody(bodydef);
+            shapedef.SetAsBox(heightX,heightY);
+        }
     }
 }
