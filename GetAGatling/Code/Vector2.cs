98a419eb5b276a0dfcd4915b2a8f2cc1e4da9be4
Gnaarf <gnaarf@gmx.de>
2015-05-08 02:29:26 +0200

    improvements on Vector2 -component-wise division -cast to Vector2u -documentation

--- GetAGatling/Code/Vector2.cs
+++ GetAGatling/Code/Vector2.cs
@@ -41,6 +41,16 @@ public struct Vector2
         return new SFML.Window.Vector2f(v.X, v.Y);
     }
 
+    public static implicit operator Vector2(SFML.Window.Vector2u v)
+    {
+        return new Vector2(v.X, v.Y);
+    }
+
+    public static implicit operator SFML.Window.Vector2u(Vector2 v)
+    {
+        return new SFML.Window.Vector2u((uint)v.X, (uint)v.Y);
+    }
+
 
     //------------------------------------------//
     //                 Constants                //
@@ -49,6 +59,22 @@ public struct Vector2
     public static Vector2 One { get { return new Vector2(1F, 1F); } }
     public static Vector2 Up { get { return new Vector2(0F, 1F); } }
     public static Vector2 Right { get { return new Vector2(1F, 0F); } }
+
+
+    //------------------------------------------//
+    //                Conversions               //
+    //------------------------------------------//
+    public Vector2 toPixelCoord()
+    {
+        return this *= GameProject2D.GameConstants.PIXEL_PER_UNIT;
+    }
+    public Vector2 PixelCoord { get { return this * GameProject2D.GameConstants.PIXEL_PER_UNIT; } }
+    
+    public Vector2 toWorldCoord()
+    {
+        return this /= GameProject2D.GameConstants.PIXEL_PER_UNIT;
+    }
+    public Vector2 WorldCoord { get { return this / GameProject2D.GameConstants.PIXEL_PER_UNIT; } }
 
 
     //------------------------------------------//
@@ -66,6 +92,7 @@ public struct Vector2
     //------------------------------------------//
     //           Static Functions               //
     //------------------------------------------//
+    /// <summary>linear interpolation by t=[0,1]</summary>
     public static Vector2 lerp(Vector2 from, Vector2 to, float t)
     {
         return (1F - t) * from + t * to;
@@ -75,37 +102,60 @@ public struct Vector2
     //           Arithmetic Operators           //
     //------------------------------------------//
     // Addition
+    /// <summary>add component-wise</summary>
     public static Vector2 operator +(Vector2 v1, Vector2 v2)
     {
         return new Vector2(v1.X + v2.X, v1.Y + v2.Y);
     }
 
     // Subtraction
+    /// <summary>subtract component-wise</summary>
     public static Vector2 operator -(Vector2 v1, Vector2 v2)
     {
         return new Vector2(v1.X - v2.X, v1.Y - v2.Y);
     }
 
     // Multiplication
+    /// <summary>multiply component-wise</summary>
     public static Vector2 operator *(Vector2 v1, Vector2 v2)
     {
         return new Vector2(v1.X * v2.X, v1.Y * v2.Y);
     }
-
+    /// <summary>multiply both components with factor</summary>
     public static Vector2 operator *(float f, Vector2 v)
     {
         return new Vector2(f * v.X, f * v.Y);
     }
-
+    /// <summary>multiply both components with factor</summary>
     public static Vector2 operator *(Vector2 v, float f)
     {
         return new Vector2(f * v.X, f * v.Y);
     }
 
     // Division
+    /// <summary>divide component-wise</summary>
+    public static Vector2 operator /(Vector2 v1, Vector2 v2)
+    {
+        return new Vector2(v1.X / v1.Y, v1.Y / v2.Y);
+    }
+    /// <summary>divide both components by factor</summary>
     public static Vector2 operator /(Vector2 v, float f)
     {
         return new Vector2(v.X / f, v.Y / f);
+    }
+
+    //------------------------------------------//
+    //           Arithmetic Operators           //
+    //------------------------------------------//
+    /// <summary>check component-wise</summary>
+    public static bool operator ==(Vector2 v1, Vector2 v2)
+    {
+        return (v1.X == v2.X) && (v1.Y == v2.Y);
+    }
+    /// <summary>check component-wise</summary>
+    public static bool operator !=(Vector2 v1, Vector2 v2)
+    {
+        return (v1.X != v2.X) || (v1.Y != v2.Y);
     }
 }
 
